cbb51dd8f1cb862d394dbd89faf3f0f47c0a5528
Anderson Cesar <andersonlscesar@gmail.com>
2022-10-17 20:35:17 -0300

    carregando piada aleatória

--- chuck-norris/public/js/main.js
+++ chuck-norris/public/js/main.js
@@ -1,7 +1,6 @@
 import chuck from "./module/chuck.js"
-import stars from "./module/stars.js"
 import jokeAPI from "./module/jokeAPI.js"
 
 // chuck()
-stars()
+
 jokeAPI()
--- chuck-norris/public/js/module/jokeAPI.js
+++ chuck-norris/public/js/module/jokeAPI.js
@@ -1,5 +1,7 @@
 export default function jokeAPI() {
 
+    // The most configs to use it on fetch API
+
     const config = {
         method: 'GET',
         headers: {
@@ -7,22 +9,56 @@ export default function jokeAPI() {
         }
     }
 
+    // The url's to reseach for categories, query or generate a random joke
+
     const URL = {
         category: 'https://api.chucknorris.io/jokes/random?category={category}',
-        query: 'https://api.chucknorris.io/jokes/search?query={query}'
+        query: 'https://api.chucknorris.io/jokes/search?query=',
+        random: 'https://api.chucknorris.io/jokes/random'
     }
-
 
     const buttonSearch = document.querySelector('.form__button')
     const inputSearch = document.querySelector('.form__input')
+    const jokeField = document.querySelector('.joke')
     const userEvents = ['click', 'touchend']
-    console.log(inputSearch)
+
+    // Loading the random joke when the document were completly loaded
+
+    window.addEventListener('load', () => {
+        generateRandomJoke(URL.random)
+    })
+ 
 
     userEvents.forEach(user => {
         buttonSearch.addEventListener(user, (e) => {
             e.preventDefault()
+            let value = inputSearch.value
+            searchForAJoke(URL.query, value)
+           
         })
     })
+
+    async function generateRandomJoke(url) {
+        const response = await fetch(url, config).then(response => response.json())
+        let joke = response.value   
+        let para = createHTMLElement(joke)
+        jokeField.appendChild( para )
+    }
+
+    async function searchForAJoke(url, value) {
+        let currentURL = url + value 
+        const response = await fetch(currentURL, config).then(response => response.json())
+        
+        console.log(response)
+    }
+
+
+    function createHTMLElement(value) {
+        let para = document.createElement('p')
+        para.classList.add('joke__para')
+        para.innerHTML = value
+        return para
+    }
 
     // async function getJoke()
 
